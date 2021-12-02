d624f07517b80f934e41c73f2faf79f79876f43c
Dennis Hedegaard <dennis@dhedegaard.dk>
2021-12-02 17:04:53 +0100

    Implemented main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::time::Instant;
+
 mod day01;
 
 fn main() {
-    println!("Hello, world!");
+    let day01_input = day01::parse_input(&day01::raw_input());
+
+    let now = Instant::now();
+    let result = day01::part1(&day01_input);
+    println!("Day 01 - part 1: {} - took: {:?}", result, now.elapsed());
+    let now = Instant::now();
+    let result = day01::part2(&day01_input);
+    println!("Day 01 - part 1: {} - took: {:?}", result, now.elapsed());
 }
