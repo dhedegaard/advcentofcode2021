781042a4a6a84d5eec46ced7fbc882d723b7f006
Dennis Hedegaard <dennis@dhedegaard.dk>
2021-12-02 16:52:59 +0100

    day01 part2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -19,20 +19,33 @@ pub fn part1(input: &[i32]) -> i32 {
     })
 }
 
+pub fn part2(input: &[i32]) -> i32 {
+    let input = input
+        .iter()
+        .enumerate()
+        .map(|(index, &elem)| {
+            if index + 2 < input.len() {
+                elem + input[index + 1] + input[index + 2]
+            } else {
+                0
+            }
+        })
+        .filter(|&e| e != 0)
+        .collect::<Vec<_>>();
+    input.iter().enumerate().fold(0, |acc, (index, &item)| {
+        if item != 0 && index >= 1 && index > 0 && input[index - 1] < item {
+            acc + 1
+        } else {
+            acc
+        }
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
+    pub const TEST_INPUT: &str = include_str!("../../inputs/day01-test.txt");
 
     #[test]
     fn examples_part1() {
@@ -44,5 +57,17 @@ mod tests {
     fn test_part() {
         let input = parse_input(&raw_input());
         assert_eq!(part1(&input), 1288);
+    }
+
+    #[test]
+    fn examples_part2() {
+        let input = parse_input(TEST_INPUT);
+        assert_eq!(part2(&input), 5);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse_input(&raw_input());
+        assert_eq!(part2(&input), 1311);
     }
 }
