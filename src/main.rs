88385cce31d58a53ef45021e01b1eeb80dfdc422
Dennis Hedegaard <dennis@dhedegaard.dk>
2021-12-02 17:17:14 +0100

    wrapping around results in main

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::time::Instant;
 
 mod day01;
 
+fn measure_and_print(day: i32, part: i32, func: &dyn Fn() -> i32) {
+    let now = Instant::now();
+    let result = func();
+    let elapsed = now.elapsed();
+    println!("Day {}, Part {}: {} ({:?}ms)", day, part, result, elapsed);
+}
+
 fn main() {
     let day01_input = day01::parse_input(&day01::raw_input());
 
-    let now = Instant::now();
-    let result = day01::part1(&day01_input);
-    println!("Day 01 - part 1: {} - took: {:?}", result, now.elapsed());
-    let now = Instant::now();
-    let result = day01::part2(&day01_input);
-    println!("Day 01 - part 2: {} - took: {:?}", result, now.elapsed());
+    measure_and_print(1, 1, &|| day01::part1(&day01_input));
+    measure_and_print(1, 2, &|| day01::part2(&day01_input));
 }
